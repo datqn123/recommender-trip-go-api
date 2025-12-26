25e4b24cdd9b89df031014a6ae81a36ede7ab264
datne123 <datmotnhit@gmail.com>
2025-12-26 12:15:19 +0700

    feat: Initialize Django project settings with database, CORS, and installed apps configuration.

--- tripgo_ai_service/settings.py
+++ tripgo_ai_service/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://tripgo-qmdo.onrender.com",
+    "https://trip-go-eight.vercel.app",
 ]
 
 # Cho phép gửi cookies/credentials (nếu cần)
